f71ae79024fc49afb36f3e916b9c9333f549a9dc
vhuang5564 <vhuang5564@gmail.com>
2021-07-25 16:16:04 +0000

    add dynamic submission/id get in /submission

--- routes/administrative.js
+++ routes/administrative.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  // Delete this later
   router.get("/", (req, res) => {
     const templateVars = {}
     res.render('administrative', templateVars);
   });
 
+  // renders administrative.ejs in administrative/id dynamically
   router.get("/:id", (req, res) => {
     const templateVars = {}
     res.render('administrative', templateVars);
--- routes/submission.js
+++ routes/submission.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  // Delete this later
   router.get("/", (req, res) => {
     const templateVars = {}
     res.render('submission', templateVars);
-
   });
+
+  // renders submission.ejs in submissions/id dynamically
+  router.get("/:id", (req, res) => {
+    const templateVars = {}
+    res.render('submission', templateVars);
+    console.log(req.params.id);
+  });
+
   return router;
 };
