106d71ee5b71770418d2a8e7b703b5c8b4b618c8
cytusalive <cytusomega@gmail.com>
2021-07-26 20:11:54 +0000

    functional form that adds poll and poll options to the database

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const administrativeBackEnd = require('./routes/administrativeBackEnd')
 const submissionBackEnd = require('./routes/submissionBackEnd')
 const indexRoutes = require('./routes/index');
 const indexBackEnd = require('./routes/indexBackEnd');
+const { generateRandomString, insertPoll, insertPollOptions } = require('./routes/helpers.js');
 
 
 
@@ -72,3 +73,38 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.post("/", (req, res) => {
+  const newLink = generateRandomString()
+  db.query(`
+    INSERT INTO poll (admin_link, submit_link, owner_email, poll_title)
+    VALUES ('http://localhost:8080/administrative/${newLink}', 'http://localhost:8080/submission/${newLink}', '${req.body['owner_email']}', '${req.body['poll_title']}');
+    `)
+  .then(() => {
+    const poll_options = req.body['poll_options'];
+    const poll_option_descs = req.body['option_desc'];
+    for (let i=0; i<poll_options.length; i++) {
+      db.query(`
+        INSERT INTO poll_options (poll_id, optionTitle, optionDesc)
+        SELECT poll.id, '${poll_options[i]}', '${poll_option_descs[i]}'
+        FROM poll
+        WHERE admin_link LIKE '%${newLink}%'
+      `)
+    }
+  })
+  .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  })
+  res.redirect('/');
+  
+  // VIEW DATABASE
+  setTimeout(() => {
+    db.query("SELECT * FROM poll")
+    .then(result => console.log(result.rows));
+    db.query("SELECT * FROM poll_options")
+    .then(result => console.log(result.rows));
+  }, 2000)
+})
+
