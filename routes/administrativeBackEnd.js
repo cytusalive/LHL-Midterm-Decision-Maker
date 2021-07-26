2e18f0e500a9fbf9cb654fddffc3849226b2705d
vhuang5564 <vhuang5564@gmail.com>
2021-07-26 11:44:35 +0000

    add helper function that inserts name in to users database when inputted

--- routes/administrativeBackEnd.js
+++ routes/administrativeBackEnd.js
@@ -1,15 +1,21 @@
+const { query } = require('express');
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
 
+
   router.post("/", (req, res) => {
+
+
     db.query(`
-    INSERT INTO users (name) VALUES ('name');
+    INSERT INTO users (name) VALUES ('${req.body}');
     `)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        console.log('data: ',data);
+        console.log('req.body ',req.body);
+        console.log('req.body.users ',req.body.users);
+        return data;
       })
       .catch(err => {
         res
