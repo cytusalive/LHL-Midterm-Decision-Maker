f7ecd085b73e86a8032cc7d3b18c942979c857e2
vhuang5564 <vhuang5564@gmail.com>
2021-07-26 15:58:56 +0000

    add insertPollOptions helper function

--- routes/helper.js
+++ routes/helper.js
@@ -2,64 +2,25 @@ const generateRandomString = () => {
   return Math.random().toString(36).substring(2, 8);
 };
 
-
-
-
-// INSERT INTO users is working
-//
-// posting from index, will take in name, email,
-router.post("/", (req, res) => {
+// insertUser argument req needs a key: value pair of name: Alice
+// code not complete need to referece user_id in poll with user.id in users
+const insertUser = (db, req, res) => {
   db.query(`
-
   INSERT INTO users (name)
   VALUES ('${req.body.name}')
-
-  INSERT INTO poll (admin_link, submit_link, owner_email)
-  VALUES (http://localhost:8080/administrative/${generateRandomString()}, http://localhost:8080/submission/${generateRandomString()}, ${req.body.email})
-
-  INSERT INTO poll_options (optionDesc)
-  VALUES (${req.body.desc})
-
-
   `)
-    .then(data => {
-      console.log('data: ',data);
-      console.log('req.body ',req.body);
-      console.log('req.body.users ',req.body.users);
-      return data;
+    .then(() => {
+      res.end()
     })
     .catch(err => {
       res
         .status(500)
-        .json({ error: err.message });
-    });
-});
+        .json({ error: err.message })
+    })
+};
 
-// DROP TABLE IF EXISTS poll CASCADE;
-// CREATE TABLE poll (
-//   id SERIAL PRIMARY KEY NOT NULL,
-//   admin_link VARCHAR(255),
-//   submit_link VARCHAR(255),
-//   owner_email VARCHAR(255),
-//   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-// );
-
-// DROP TABLE IF EXISTS poll_options CASCADE;
-// CREATE TABLE poll_options (
-//   id SERIAL PRIMARY KEY NOT NULL,
-//   poll_id INTEGER REFERENCES poll(id) ON DELETE CASCADE,
-//   optionDesc TEXT
-// );
-
-// DROP TABLE IF EXISTS poll_options CASCADE;
-// CREATE TABLE poll_options (
-//   id SERIAL PRIMARY KEY NOT NULL,
-//   poll_id INTEGER REFERENCES poll(id) ON DELETE CASCADE,
-//   optionDesc TEXT
-// );
-
-
-// insertPoll argument req needs a key: value pair of  email: example@example.com
+// insertPoll argument req needs a key: value pair of email: example@example.com
+// code not complete need to reference user_id in poll to user.id in users
 const insertPoll = (db, req, res) => {
   db.query(`
     INSERT INTO poll (admin_link, submit_link, owner_email)
@@ -75,4 +36,19 @@ const insertPoll = (db, req, res) => {
     })
 }
 
-
+// insertPollOptions req needs a key: value pair of title: Titanic and desc: Sank
+// code not complete need to reference poll_id in pollOptions with poll.id in polls
+const insertPollOptions = (db, req, res) => {
+  db.query(`
+  INSERT INTO poll_options (optionTitle, optionDesc)
+  VALUES ('${req.body.title}', '${req.body.desc}')
+  `)
+    .then(() => {
+      res.end();
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    })
+}
