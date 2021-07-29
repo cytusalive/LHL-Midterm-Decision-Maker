1fcb5e5a08c2d4902e9e91ac30bd9d7deab80107
cytusalive <cytusomega@gmail.com>
2021-07-29 02:39:23 +0000

    also pass poll title to render

--- routes/submission.js
+++ routes/submission.js
@@ -5,11 +5,12 @@ module.exports = (db) => {
 
   // renders submission.ejs in submissions/id dynamically
   router.get("/:id", (req, res) => {
-    db.query(`SELECT id FROM poll WHERE submit_link LIKE '%${req.params.id}%'`)
+    db.query(`SELECT id, poll_title FROM poll WHERE submit_link LIKE '%${req.params.id}%'`)
     .then (result => {
+      const poll_title = result.rows[0].poll_title;
       db.query(`SELECT * FROM poll_options WHERE poll_id = ${result.rows[0].id}`)
       .then(result => {
-        const templateVars = {options: result.rows};
+        const templateVars = {options: result.rows, poll_title: poll_title};
         console.log(templateVars.options)
         res.render('submission', templateVars);
       });
